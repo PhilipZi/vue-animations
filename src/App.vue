08002bf333523a2a8d096dd04f46dc682e159d4d
Philip Zimmermann <philip.zimmermann@freenet.de>
2023-04-04 10:27:34 +0200

    add css classname transitions

--- src/App.vue
+++ src/App.vue
@@ -25,7 +25,11 @@
   <button @click="addItem">Add</button>
 
   <ul>
-    <TransitionGroup name="fade">
+    <TransitionGroup
+      name="fade"
+      enter-active-class="animate__animated animate__flipInX"
+      leave-active-class="animate__animated animate__flipOutX"
+    >
       <li
         v-for="(number, index) in numbers"
         :key="number"
@@ -93,6 +97,14 @@ export default {
 </script>
 
 <style>
+.animate__flipOutX {
+  position: absolute;
+}
+
+.animate__animated {
+  animation-duration: 1.5s;
+}
+
 li {
   font-size: 22px;
   cursor: pointer;
